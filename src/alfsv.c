a606d63968e4be9707c2050684e9e143187b3a1d
Fabian Klötzl <fabian@kloetzl.info>
2015-09-02 17:01:11 +0200

    computes quads

--- src/alfsv.c
+++ src/alfsv.c
@@ -8,6 +8,8 @@
 #include "matrix.h"
 #include "graph.h"
 
+int quadruple_stats(matrix *distance, tree_node *root);
+
 int main(int argc, const char *argv[]) {
 
 	if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'h') {
@@ -41,6 +43,8 @@ int main(int argc, const char *argv[]) {
 		neighbor_joining(&distance, &root);
 		newick(&root);
 
+		quadruple_stats(&distance, &root);
+
 		fclose(file_ptr);
 	}
 
@@ -59,10 +63,67 @@ int quadruple_stats(matrix *distance, tree_node *root) {
 	types = malloc(size);
 	memset(types, SET_D, size);
 
-	type= SET_A ; consume(root->left_branch->left_branch);
-	type= SET_B ; consume(root->left_branch->right_branch);
-	type= SET_C ; consume(root->right_branch);
+	type = SET_A;
+	consume(root->left_branch->left_branch);
+	type = SET_B;
+	consume(root->left_branch->right_branch);
+	type = SET_C;
+	consume(root->right_branch);
 	// D = not A, B, C;
+
+	size_t set_counts[4] = {0};
+	for (size_t i = 0; i < size; i++) {
+		set_counts[(int)types[i]]++;
+	}
+
+	printf("%zu %zu %zu %zu\n", set_counts[SET_A], set_counts[SET_B],
+	       set_counts[SET_C], set_counts[SET_D]);
+
+	size_t non_supporting_counter = 0;
+
+	size_t A = 0, B, C, D;
+	for (; A < size; A++) {
+		for (; types[A] != SET_A && A < size; A++)
+			;
+		if (A >= size) {
+			break;
+		}
+
+		for (B = 0; B < size; B++) {
+			for (; types[B] != SET_B && B < size; B++)
+				;
+			if (B >= size) {
+				break;
+			}
+
+			for (C = 0; C < size; C++) {
+				for (; types[C] != SET_C && C < size; C++)
+					;
+				if (C >= size) {
+					break;
+				}
+
+				for (D = 0; D < size; D++) {
+					for (; types[D] != SET_D && D < size; D++)
+						;
+					if (D >= size) {
+						break;
+					}
+
+#define M(I, J) (MATRIX_CELL(*distance, I, J))
+					printf("%zu %zu %zu %zu\n", A, B, C, D);
+
+					double D_abcd = M(A, B) + M(C, D);
+					if (M(A, C) + M(B, D) < D_abcd ||
+					    M(A, D) + M(B, C) < D_abcd) {
+						non_supporting_counter++;
+					}
+				}
+			}
+		}
+	}
+
+	printf("%zu\n", non_supporting_counter);
 }
 
 void consume_process(tree_node *current) {
@@ -72,7 +133,8 @@ void consume_process(tree_node *current) {
 }
 
 void consume(tree_node *current) {
+	if (!current) return;
 	visitor v = {.pre = NULL, .process = consume_process, .post = NULL};
 
-	traverse(current, consume_process);
+	traverse(current, &v);
 }
